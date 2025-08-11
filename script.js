c0c3c96e52880495a7932113fa8620f68df63910
Alan-campos17 <149601081+Alan-campos17@users.noreply.github.com>
2025-08-11 16:38:34 -0300

    arrumando o error do botao

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ function botaoMostraPalavrasChave(){
     const texto = document.querySelector("#entrada-de-texto").Value;
     const campoResultado = document.querySelector("#resultado-palavrchave");
     const palavrasChave = processaTexto(texto);
-    campoResultado.textContent = palavraChave.join(",");
+    campoResultado.textContent = palavrasChave.join(",");
 
 }
 
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
-    const frequencias = contraFrequencia(palavras);
-    let ordenadas = Object.keys(frequencias).sort(ordenadaPalavras);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavras);
 
     function ordenaPalavras(p1, p2){
         return frequencias[p2] - frequencias[p1];
