758e469b2c8a8d5e1b72a7f0c56fd59fd398a118
Alan-campos17 <149601081+Alan-campos17@users.noreply.github.com>
2025-08-11 16:11:04 -0300

    salvando projeto

---
+++ script.js
@@ -0,0 +1,8 @@
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
+botaoMostraPalavras.addEventListener("click", botaoMostraPalavras);
+
+function botaoMostraPalavrasChave(){
+    const texto = document.querySelector("#entrada-de-texto").Value;
+    const campoResultado = document.querySelector
+}
